5b16a1cc0b0b47de80efc1d5dd06a7aa21badb89
MateuszRabijasz <107041899+MateuszRabijasz@users.noreply.github.com>
2025-01-29 18:02:52 +0100

    Add files via upload

---
+++ Lab4zad1.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+printf "Podaj wagę (kg): "
+let weight = Console.ReadLine() |> float
+printf "Podaj wzrost (cm): "
+let height = Console.ReadLine() |> float
+
+let dzielnik = 100.0
+let waga = height / dzielnik
+let bmi = weight / (waga * waga)
+
+let category =
+    if bmi < 18.5 then "Niedowaga"
+    elif bmi < 24.9 then "Prawidłowa waga"
+    elif bmi < 29.9 then "Nadwaga"
+    else "Otyłość"
+
+printfn "Twoje BMI: %.2f" bmi
+printfn "Kategoria BMI: %s" category
