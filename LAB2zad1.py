2243eb6cd4f0e90a43330250552c9d572d2f1786
MateuszRabijasz <107041899+MateuszRabijasz@users.noreply.github.com>
2025-01-30 20:50:15 +0100

    Update LAB2zad1.py

--- LAB2zad1.py
+++ LAB2zad1.py
@@ -10,13 +10,13 @@ def zadanie1(text):
     print(f"Liczba zdań: {ilosc_zdan}")
     print(f"Liczba akapitów: {ilosc_akapitow}")
 
-    # Najczęstsze słowa bez stop words
+
     stop_words = {"i", "a", "the", "ale", "nie", "na", "to"}
     words = [word.strip(".,").lower() for word in text.split() if word.lower() not in stop_words]
     najczestsze = Counter(words).most_common(3)
     print("Najczęstsze słowa (bez stop words):", najczestsze)
 
-    # Transformacja słów zaczynających się na "a" lub "A"
+
     transformed = [
         word[::-1] if word.lower().startswith("a") else word
         for word in text.split()
@@ -26,7 +26,7 @@ def zadanie1(text):
     print(transformed_text)
 
 
-# Przykładowy tekst
+
 tekst = """
 Ala ma kota.
 Ala ma kota. Ale nie ma psa.
